592046f8040c04015224164c756180da83a802e7
yinquan <yinquan@yinquan-pc.tinywisp>
2025-05-06 00:14:01 +0800

    Initial commit

---
+++ src/editor/VNodeRenderer.vue
@@ -0,0 +1,15 @@
+<script>
+import { defineComponent, h } from 'vue'
+
+export default defineComponent({
+  props: {
+    vnodes: {
+      type: Object,
+      required: true,
+    },
+  },
+  render() {
+    return this.vnodes.length !== 1 ? h('div', {}, this.vnodes) : this.vnodes[0]
+  },
+})
+</script>
